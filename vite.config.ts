4a3593e3fa00a068bbea7726372c33dc7e31ed16
Tim Voss <93534907+timrecursify@users.noreply.github.com>
2024-10-24 16:48:32 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
+    outDir: 'dist',
     rollupOptions: {
       output: {
         entryFileNames: 'assets/[name].[hash].js',
@@ -13,4 +13,10 @@ export default defineConfig({
       },
     },
   },
+  server: {
+    cors: true
+  },
+  optimizeDeps: {
+    exclude: ['js-big-decimal']
+  }
 });
